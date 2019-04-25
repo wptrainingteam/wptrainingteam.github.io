f11cd49a7a196a23e8b3f2458e8689f43ad07add
Jesse C. Owens <jesse@jessecowens.com>
2019-04-25 14:57:06 -0600

    Quote variables for safety

--- .tools/update-lessons.sh
+++ .tools/update-lessons.sh
@@ -1,20 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-OMIT=omitted-repos.manifest
-ALL=all-repos.manifest
-LESSONLIST=lesson-plans.manifest
-LESSONPATH=../lesson-plan/
+REPO="https://api.github.com/orgs/wptrainingteam"
+OMIT="omitted-repos.manifest"
+ALL="all-repos.manifest"
+LESSONLIST="lesson-plans.manifest"
+LESSONPATH="../lesson-plan/"
 
-curl "https://api.github.com/orgs/wptrainingteam/repos?per_page=1000" | grep -w clone_url | grep -o '[^"]\+://.\+.git' > $ALL
+curl "$REPO/repos?per_page=1000" |
+    grep -w clone_url |
+    grep -o '[^"]\+://.\+.git' > "$ALL"
 
-awk 'NR == FNR { list[$0]=1; next } { if (! list[$0]) print }' $OMIT $ALL > $LESSONLIST
+awk 'NR == FNR { list[$0]=1; next } { if (! list[$0]) print }' "$OMIT" "$ALL" > "$LESSONLIST"
 
-LESSONS=$(cat $LESSONLIST)
-cd $LESSONPATH
+LESSONS="$(cat $LESSONLIST)"
+cd "$LESSONPATH"
 
-for lesson in $LESSONS;
-do
-	git submodule add -b master $lesson || continue
+for lesson in $LESSONS; do
+    git submodule add -b master "$lesson" || continue
 done
 
 git submodule foreach git submodule init
