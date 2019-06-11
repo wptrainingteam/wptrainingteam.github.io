d43a8be6db89c5ee5fb351accdd1c4bb578543b1
Jesse C. Owens <jesse@jessecowens.com>
2019-06-11 06:42:17 -0600

    Fix json format round 3

--- .tools/update-manifest.sh
+++ .tools/update-manifest.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")"
 
 LESSONLIST="lesson-plans.manifest"
-LESSONPLANROOT="https://wptrainingteam.github.io/lesson-plan/"
+LESSONPLANROOT="https:\/\/github.com\/wptrainingteam\/"
 MANIFEST="../manifest.json"
 
 printf "{\n" > $MANIFEST
@@ -15,7 +15,7 @@ do
   friendly_title=$(echo $rawtitle | sed 's/-/ /g');
   friendly_title=$(echo $friendly_title | awk '{for(i=1;i<=NF;i++)sub(/./,toupper(substr($i,1,1)),$i)}1');
   #echo $friendly_title;
-  markdown_source="$LESSONPLANROOT$rawtitle/README.md";
+  markdown_source="$LESSONPLANROOT$rawtitle\/blob\/master\/README.md";
   #echo $markdown_source;
   printf "\t\"$rawtitle\": {\n" >> $MANIFEST;
   printf "\t\t\"title\": \"$friendly_title\",\n" >> $MANIFEST;
